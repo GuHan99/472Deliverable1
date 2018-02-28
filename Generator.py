7b8f60ca46e9761d9726e933929fa090d6072c1c
HAN GU <GU_HAN@guangs-mbp.wireless.concordia.ca>
2018-02-27 23:39:58 -0500

    add

---
+++ Generator.py
@@ -0,0 +1,42 @@
+import random
+
+pattern = ['1 1', '2 1', '3 1', '4 1']
+
+def get_pattern(level):
+    level_array = []
+    if level is '1':
+        level_array = ['r', 'r', 'r', 'r', 'r', 'r', 'b', 'b', 'b', 'b', 'b', 'b', 'w', 'w', 'e']
+    elif level is '2':
+        level_array = ['r', 'r', 'r', 'r', 'r', 'r', 'b', 'b', 'b', 'b', 'w', 'w', 'y', 'y', 'e']
+    elif level is '3':
+        level_array = ['r', 'r', 'r', 'r', 'b', 'b', 'b', 'b', 'w', 'w', 'y', 'y', 'g', 'g', 'e']
+    elif level is '4':
+        level_array = ['r', 'r', 'r', 'r', 'b', 'b', 'w', 'w', 'y', 'y', 'g', 'g', 'p', 'p', 'e']
+
+    list = {}
+    for i in level_array:
+        random_num = random.randint(0, 14)
+        while random_num in list.keys():
+            random_num = random.randint(0, 14)
+        list[random_num] = i
+    sort_list = sorted(list.items())
+    result = []
+    for i in sort_list:
+        result.append(i[1])
+    result_ = ' '.join(result)
+
+    return result_
+
+
+sample_result = ''
+for i in pattern:
+    level = i.split(' ')[0]
+    amount = int(i.split(' ')[1])
+    for x in range(amount):
+        result = get_pattern(level=level)
+        sample_result += result + '\n'
+
+with open('./sample_level.txt', 'w+') as f:
+    f.write(sample_result)
+
+
